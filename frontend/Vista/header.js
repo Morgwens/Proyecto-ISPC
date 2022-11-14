671077da913b504afa078273719b8059be186ab6
Ignacio Joaquín Soler <isoler@ia.com.mx>
2022-11-14 13:25:22 -0300

    delete

--- frontend/Vista/header.js
+++
@@ -1,6 +0,0 @@
-const checkBtn = document.querySelector('.check-btn');
-const sideBar = document.querySelector('.nav');
-
-checkBtn.addEventListener('click', () => {
-    sideBar.classList.contains('sidebar-on') ? sideBar.classList.remove('sidebar-on') : sideBar.classList.add('sidebar-on')
-});
